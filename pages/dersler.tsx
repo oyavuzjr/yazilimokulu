1ede9604ba15c93470b3b7a143669087bf202fe8
oyavuzjr <oyavuz@g.ucla.edu>
2022-08-30 19:58:41 +0300

    directory scan

--- pages/dersler.tsx
+++ pages/dersler.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import fs from 'fs';
+import path from 'path';
 type Props = {};
 
 export default function Desler({}: Props) {
@@ -7,12 +8,17 @@ export default function Desler({}: Props) {
 }
 
 export async function getStaticPaths() {
-  const courses = fs.readdirSync('.');
+  const coursesPath = path.join(process.cwd(), 'dersler');
+  const courses = fs
+    .readdirSync(coursesPath, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
   console.log('@@@@@@@@', courses);
 
   return {
     paths: {
-      params: [{ id: 1 }],
+      params: [{ id: '1' }],
     },
     fallback: false,
   };
