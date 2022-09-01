a131e769912640902311067eb80cafb17f750552
oyavuzjr <oyavuz@g.ucla.edu>
2022-09-01 16:21:18 +0300

    sidebar bg color

--- app/src/components/layouts/SideBar/index.tsx
+++ app/src/components/layouts/SideBar/index.tsx
@@ -176,7 +176,7 @@ export default function SideBar({
         {/* Static sidebar for desktop */}
         <div className="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex flex-grow flex-col overflow-y-auto bg-blue-700 pt-5 ">
+          <div className="flex flex-grow flex-col overflow-y-auto bg-blue-800 pt-5 ">
             <div className="flex flex-shrink-0 items-center px-4">
               <div className=" w-auto">{topOfBar}</div>
             </div>
