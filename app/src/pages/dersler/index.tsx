340aded8b2ece5e003ffae990815ac9f773a642b
oyavuzjr <oyavuz@g.ucla.edu>
2022-09-02 07:32:49 +0300

    added links to courses in courses page

--- app/src/pages/dersler/index.tsx
+++ app/src/pages/dersler/index.tsx
@@ -5,6 +5,7 @@ import Header from '../../components/Header';
 import { GetStaticProps } from 'next';
 import { Course, PrismaClient } from '@prisma/client';
 import Breadcrumbs from '../../components/Breadcrumbs';
+import Link from 'next/link';
 /*
   This example requires Tailwind CSS v2.0+ 
   
@@ -64,31 +65,33 @@ export default function Courses({ courses }: Props) {
           className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
         >
           {courses.map((course, idx) => (
-            <li key={idx} className="relative">
-              <div className="w-40 h-40 group aspect-square block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-                <img
-                  src={
-                    'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/1200px-Python.svg.png'
-                  }
-                  alt=""
-                  className="pointer-events-none object-cover group-hover:opacity-75"
-                />
-                <button
-                  type="button"
-                  className="absolute inset-0 focus:outline-none"
-                >
-                  <span className="sr-only">
-                    View details for {course.name}
-                  </span>
-                </button>
-              </div>
-              <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
-                {course.name}
-              </p>
-              <p className="pointer-events-none block text-sm font-medium text-gray-500">
-                {course.slug}
-              </p>
-            </li>
+            <Link key={idx} href={`/dersler/${course.slug}`}>
+              <li key={idx} className="relative">
+                <div className="w-40 h-40 group aspect-square block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+                  <img
+                    src={
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/1200px-Python.svg.png'
+                    }
+                    alt=""
+                    className="pointer-events-none object-cover group-hover:opacity-75"
+                  />
+                  <button
+                    type="button"
+                    className="absolute inset-0 focus:outline-none"
+                  >
+                    <span className="sr-only">
+                      View details for {course.name}
+                    </span>
+                  </button>
+                </div>
+                <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
+                  {course.name}
+                </p>
+                <p className="pointer-events-none block text-sm font-medium text-gray-500">
+                  {course.slug}
+                </p>
+              </li>
+            </Link>
           ))}
         </ul>
       </div>
